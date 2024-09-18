2c189c47f5a804a5ee71b0d54ea81de4f8653714
Kurniawan Jatmika <Kurniawan.Jatmika@nawadata.com>
2024-09-18 16:35:48 +0700

    Add project files.

---
+++ ResourceUsage/Default.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ResourceUsage.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        table {
+            border-collapse: collapse;
+            border: 1px solid black;
+        }
+        table td, table th {
+            border: 1px solid black;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server" style="font-family: sans-serif">
+        <asp:ScriptManager runat="server" />
+        <asp:UpdatePanel runat="server">
+            <ContentTemplate>
+                <table>
+                    <tr>
+                        <th>Parameter</th>
+                        <th>Value</th>
+                    </tr>
+                    <tr>
+                        <td style="font-weight:bold">CPU Usage</td>
+                        <td style="text-align: right"><asp:Label ID="lblCPU" runat="server" /></td>
+                    </tr>                    
+                    <tr>
+                        <td style="font-weight:bold">Memory Usage</td>
+                        <td style="text-align: right"><asp:Label ID="lblRamUsage" runat="server" /></td>
+                    </tr>                    
+                    <tr>
+                        <td style="font-weight:bold">Memory Available / Total</td>
+                        <td style="text-align: right"><asp:Label ID="lblRam" runat="server" /></td>
+                    </tr>
+                </table>
+                <br />
+                <asp:Timer runat="server" OnTick="OnTick" Interval="1000"></asp:Timer>
+            </ContentTemplate>
+        </asp:UpdatePanel>
+    </form>
+</body>
+</html>
---
+++ ResourceUsage/Default.aspx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+using System.Text;
+using System.IO;
+
+namespace ResourceUsage
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected static PerformanceCounter CPUCounter;
+        protected static PerformanceCounter availableMemoryCounter;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                //PrintPerformanceCounterParameters();
+
+                CPUCounter = new PerformanceCounter();
+                CPUCounter.CategoryName = "Processor";
+                CPUCounter.CounterName = "% Processor Time";
+                CPUCounter.InstanceName = "_Total";
+
+                availableMemoryCounter = new PerformanceCounter();
+                availableMemoryCounter.CategoryName = "Memory";
+                availableMemoryCounter.CounterName = "Available KBytes";
+            }
+        }
+
+        protected void OnTick(object sender, EventArgs e)
+        {
+            float cpuUsage = GetCpuUsage();
+            float availableMemory = GetAvailableMemory()/1024;
+            float totalMemory = GetTotalMemory()/1024/1024;
+
+            // Check the if condition for value greater than 90.
+            lblCPU.Text = cpuUsage.ToString("N2") + " %";
+            lblRamUsage.Text = ((totalMemory-availableMemory) / totalMemory * 100).ToString("N2") + " %";
+            lblRam.Text = availableMemory.ToString("N0") + " MB / " + totalMemory.ToString("N0") + " MB";
+        }
+
+        public float GetCpuUsage()
+        {
+            return CPUCounter.NextValue();
+        }
+
+        public float GetAvailableMemory()
+        {
+            return availableMemoryCounter.NextValue();
+        }
+
+        public float GetTotalMemory()
+        {
+            return new Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory;
+        }
+
+        private static void PrintPerformanceCounterParameters()
+        {
+            var sb = new StringBuilder();
+            PerformanceCounterCategory[] categories = PerformanceCounterCategory.GetCategories();
+
+            var desiredCategories = new HashSet<string> { "Process", "Memory" };
+
+            foreach (var category in categories)
+            {
+                sb.AppendLine("Category: " + category.CategoryName);
+                if (desiredCategories.Contains(category.CategoryName))
+                {
+                    PerformanceCounter[] counters;
+                    try
+                    {
+                        counters = category.GetCounters("devenv");
+                    }
+                    catch (Exception)
+                    {
+                        counters = category.GetCounters();
+                    }
+
+                    foreach (var counter in counters)
+                    {
+                        sb.AppendLine(counter.CounterName + ": " + counter.CounterHelp);
+                    }
+                }
+            }
+            File.WriteAllText(@"C:\performanceCounters.txt", sb.ToString());
+        }
+    }
+}
